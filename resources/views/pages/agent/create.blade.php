dfa63e4450ee178d8a91ad7a76217da29001dc46
hassasultan <h.hsultan277@gmail.com>
2024-07-23 15:00:34 +0500

    pushed

--- resources/views/pages/agent/create.blade.php
+++ resources/views/pages/agent/create.blade.php
@@ -1,83 +1,154 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <div class="card my-4">
-                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                    <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-                        <div class="row">
-                            <div class="col-6">
-                                <h6 class="text-white text-capitalize ps-3">Add Mobile Agent</h6>
+    <style>
+        .skeleton-row {
+            background-color: #f2f2f2;
+        }
+
+        .skeleton-row td {
+            height: 20px;
+            /* Adjust height as needed */
+            border: none;
+        }
+    </style>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                {{-- @include('layouts.include.toolbar') --}}
+                <h2 class="page-title">Agent Management</h2>
+                <div class="row">
+                    <div class="col-md-12 my-4">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h5>
+                                        Add New Agent </h5>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional
+                                        content.</p>
+                                </div>
+                                <div class="toolbar">
+                                    {{-- <form class="form">
+                                    <div class="form-row">
+                                        <div class="form-group col-auto mr-auto">
+                                        </div>
+                                        <div class="form-group col-auto">
+                                            <label for="search" class="sr-only">Search</label>
+                                            <input type="text" class="form-control" id="search1" value=""
+                                                placeholder="Search">
+                                        </div>
+                                    </div>
+                                </form> --}}
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <h5>Give Agent Informarion...</h5>
+                                        <form role="form" method="POST" action="{{ route('agent-management.store') }}"
+                                            enctype="multipart/form-data">
+                                            @csrf
+                                            <div class="row">
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Select User*</label>
+                                                    <select name="user_id"
+                                                        class="select2 form-control fs-14  h-50px" required>
+                                                        @foreach ($user as $row)
+                                                            <option value="{{ $row->id }}">{{ $row->name }}
+                                                                ({{ $row->email }})</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Select Project*</label>
+                                                    <select name="town_id" id="town_id"
+                                                        class="select2 form-control fs-14  h-50px" required>
+                                                        @foreach ($town as $row)
+                                                            <option value="{{ $row->id }}">{{ $row->town }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Select Project Location*</label>
+                                                    <select name="sub_town_id" id="sub_town_id"
+                                                        class="select2 form-control fs-14  h-50px" required>
+                                                        {{-- @foreach ($subtown as $row)
+                                                            <option value="{{ $row->id }}">({{ $row->town->town }})
+                                                                {{ $row->title }}</option>
+                                                        @endforeach --}}
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Select Type*</label>
+                                                    <select name="type_id"
+                                                        class="select2 form-control fs-14  h-50px" required>
+                                                        @foreach ($type as $row)
+                                                            <option value="{{ $row->id }}">({{ $row->title }})
+                                                            </option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Agent Profile</label>
+                                                    <input type="file"
+                                                        class="form-control border-bottom border-1 border-dark"
+                                                        name="avatar" value="{{ old('avatar') }}" required />
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Description</label>
+                                                    <textarea class="form-control border-bottom border-1 border-dark" placeholder="Enter Desciption..." name="description"
+                                                        value="{{ old('description') }}" required></textarea>
+
+                                                </div>
+                                                <div class="form-group col-12 p-3">
+                                                    <label>Address*</label>
+                                                    <input type="text"
+                                                        class="form-control border-bottom border-1 border-dark"
+                                                        placeholder="Enter Address Here..." name="address" required
+                                                        value="{{ old('address') }}" />
+                                                </div>
+
+
+                                                <div class="form-group col-12 p-3 text-right">
+
+                                                    <button type="submit"
+                                                        class="btn btn-primary btn-lg w-20 mt-4 mb-0">Create</button>
+                                                </div>
+                                            </div>
+
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card-body px-4 pb-2">
-                    <h5>Give Agent Informarion...</h5>
-                    <form role="form" method="POST" action="{{ route('agent-management.store') }}"
-                        enctype="multipart/form-data">
-                        @csrf
-                        <div class="row">
-                            <div class="form-group col-12 p-3">
-                                <label>Select User*</label>
-                                <select name="user_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($user as $row)
-                                        <option value="{{ $row->id }}">{{ $row->name }} ({{ $row->email }})</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select Town*</label>
-                                <select name="town_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($town as $row)
-                                        <option value="{{ $row->id }}">{{ $row->town }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select SubTown*</label>
-                                <select name="sub_town_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($subtown as $row)
-                                        <option value="{{ $row->id }}">({{ $row->town->town }})  {{ $row->title }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select Type*</label>
-                                <select name="type_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($type as $row)
-                                        <option value="{{ $row->id }}">({{ $row->title }})</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Agent Profile</label>
-                                <input type="file" class="form-control border-bottom border-1 border-dark"
-                                     name="avatar" value="{{ old('avatar') }}" required />
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Description</label>
-                                <textarea class="form-control border-bottom border-1 border-dark"
-                                    placeholder="Enter Desciption..." name="description" value="{{ old('description') }}" required ></textarea>
-
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Address*</label>
-                                <input type="text" class="form-control border-bottom border-1 border-dark"
-                                placeholder="Enter Address Here..." name="address" required  value="{{ old('address') }}"/>
-                            </div>
-
-
-                            <div class="text-center">
-                                <button type="submit"
-                                    class="btn btn-lg bg-gradient-primary btn-lg w-20 mt-4 mb-0">Create</button>
-                            </div>
-                        </div>
-
-                    </form>
-                </div>
             </div>
         </div>
     </div>
+@endsection
+@section('bottom_script')
+    <script>
+        $("#town_id").on("change", function() {
+            var town_id = $(this).val();
+            $.ajax({
+                type: "get",
+                url: "{{ route('subtown.by.town') }}",
+                data: {
+                    'town_id': town_id,
+                },
+                success: function(data) {
+                    $("#sub_town_id").html("");
+                    var your_html = "";
+                    $.each(data, function(key, val) {
+                        console.log(val);
+                        your_html += "<option value=" + val['id'] + ">" + val['title'] +
+                            "</option>"
+                    });
+                    $("#sub_town_id").append(your_html); //// For Append
+                },
+                error: function() {
+                    console.log(data);
+                }
+            });
+        });
+    </script>    
 @endsection
--- resources/views/pages/agent/index.blade.php
+++ resources/views/pages/agent/index.blade.php
@@ -1,78 +1,129 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        .skeleton-row {
+            background-color: #f2f2f2;
+        }
 
-<div class="row">
-    <div class="col-12">
-      <div class="card my-4">
-        <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-          <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-            <div class="row">
-                <div class="col-6">
-                    <h6 class="text-white text-capitalize ps-3">Agent List</h6>
+        .skeleton-row td {
+            height: 20px;
+            /* Adjust height as needed */
+            border: none;
+        }
+    </style>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                {{-- @include('layouts.include.toolbar') --}}
+                <h2 class="page-title">Agent Management</h2>
+                <p> Tables with built-in bootstrap styles </p>
+                <div class="col-12 text-right">
+                    <a class="btn btn-primary" href="{{ route('agent-management.create') }}">add</i>&nbsp;&nbsp;<i
+                            class="fa fa-user"></i></a>
                 </div>
-                <div class="col-6 text-end">
-                    <a class="btn bg-gradient-dark mb-0 mr-3" href="{{ route('agent-management.create') }}"><i class="material-icons text-sm">add</i>&nbsp;&nbsp;<i class="fa fa-user"></i></a>
+                <div class="row">
+                    <div class="col-md-12 my-4">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <div class="card-title">
+                                    <h5>
+                                        Agent List
+                                    </h5>
+                                    <p class="card-text">With supporting text below as a natural lead-in to additional
+                                        content.</p>
+                                </div>
+                                <div class="toolbar">
+                                    {{-- <form class="form">
+                                        <div class="form-row">
+                                            <div class="form-group col-auto mr-auto">
+                                            </div>
+                                            <div class="form-group col-auto">
+                                                <label for="search" class="sr-only">Search</label>
+                                                <input type="text" class="form-control" id="search1" value=""
+                                                    placeholder="Search">
+                                            </div>
+                                        </div>
+                                    </form> --}}
+                                </div>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <table id="example1" class="table table-borderless table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th
+                                                        class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                                        Agent</th>
+                                                    <th
+                                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                        Town</th>
+                                                    <th
+                                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                        Type</th>
+                                                    <th
+                                                        class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                                        Address</th>
+                                                    {{-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Trucks</th> --}}
+                                                    <th class="text-secondary opacity-7">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {{-- @if (count($user) > 0) --}}
+                                                @foreach ($agent as $key => $row)
+                                                    <tr>
+                                                        <td>
+                                                            <p class="text-xs font-weight-bold mb-0">{{ $row->user->name }}
+                                                            </p>
+                                                            @if ($row->avatar != null)
+                                                                <img src="{{ asset('storage/' . $row->avatar) }}"
+                                                                    class="img-fluid" style="width: 70px; height: 70px;" />
+                                                            @endif
+                                                        </td>
+                                                        <td class="align-middle text-center text-sm">
+                                                            <p class="text-xs text-secondary mb-0">{{ $row->town->town }}
+                                                            </p>
+                                                            <p class="text-xs text-secondary mb-0">{{ $row->town->subtown }}
+                                                            </p>
+                                                        </td>
+                                                        <td class="align-middle text-center text-sm">
+                                                            <p class="text-xs text-secondary mb-0">
+                                                                {{ $row->complaint_type->title ?? 'Type is not defined...' }}
+                                                            </p>
+                                                        </td>
+                                                        <td>
+                                                            <p class="text-xs text-center font-weight-bold mb-0">
+                                                                {{ $row->address }} </p>
+                                                        </td>
+                                                        {{-- <td class="align-middle text-center text-sm">
+                                                    <p class="text-xs text-secondary mb-0">{{ count($row->hydrant->vehicles) }}</p>
+                                                </td> --}}
+                                                        <td class="align-middle">
+                                                            <a href="{{ route('agent-management.edit', $row->id) }}"
+                                                                class="text-secondary font-weight-bold text-xs m-3"
+                                                                data-toggle="tooltip" data-original-title="Edit user">
+                                                                Edit
+                                                            </a>
+                                                            |
+                                                            <a href="{{ route('agent-management.details', $row->id) }}"
+                                                                class="text-secondary font-weight-bold text-xs m-3"
+                                                                data-toggle="tooltip" data-original-title="Edit user">
+                                                                Assigned Complaints
+                                                            </a>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                                {{-- @else
+                                        No Record Find...
+                                    @endif --}}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
         </div>
-        <div class="card-body px-0 pb-2">
-          <div class="p-0">
-            <table id="example1" class="table table-bordered align-items-center mb-0">
-              <thead>
-                <tr>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Agent</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Town</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Type</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Address</th>
-                  {{-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Trucks</th> --}}
-                  <th class="text-secondary opacity-7">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {{-- @if(count($user) > 0) --}}
-                    @foreach ($agent as $key => $row)
-                        <tr>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $row->user->name }}</p>
-                                @if ($row->avatar != NULL)
-                                    <img src="{{ asset('storage/'.$row->avatar) }}" class="img-fluid" style="width: 70px; height: 70px;"/>
-                                @endif
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                <p class="text-xs text-secondary mb-0">{{ $row->town->town }}</p>
-                                <p class="text-xs text-secondary mb-0">{{ $row->town->subtown }}</p>
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                <p class="text-xs text-secondary mb-0">{{ $row->complaint_type->title ?? 'Type is not defined...' }}</p>
-                            </td>
-                            <td>
-                                <p class="text-xs text-center font-weight-bold mb-0"> {{ $row->address }} </p>
-                            </td>
-                            {{-- <td class="align-middle text-center text-sm">
-                                <p class="text-xs text-secondary mb-0">{{ count($row->hydrant->vehicles) }}</p>
-                            </td> --}}
-                            <td class="align-middle">
-                                <a href="{{ route('agent-management.edit',$row->id) }}" class="text-secondary font-weight-bold text-xs m-3" data-toggle="tooltip" data-original-title="Edit user">
-                                Edit
-                                </a>
-                                |
-                                <a href="{{ route('agent-management.details',$row->id) }}" class="text-secondary font-weight-bold text-xs m-3" data-toggle="tooltip" data-original-title="Edit user">
-                                    Assigned Complaints
-                                    </a>
-                            </td>
-                        </tr>
-                    @endforeach
-                {{-- @else
-                    No Record Find...
-                @endif --}}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
     </div>
-</div>
-
 @endsection
