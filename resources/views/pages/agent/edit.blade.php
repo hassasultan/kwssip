e8546c1eee94d049aa84b9d63e738847d793f9a3
hassasultan <96184043+hassasultan@users.noreply.github.com>
2025-02-27 15:29:20 +0500

    pushed

--- resources/views/pages/agent/edit.blade.php
+++ resources/views/pages/agent/edit.blade.php
@@ -16,76 +16,83 @@
                 <div class="card-body px-4 pb-2">
                     <h5>Update Agent Informarion...</h5>
                     <hr />
-                    <form role="form" method="POST" action="{{ route('agent-management.update', $agent->id) }}"
-                        enctype="multipart/form-data">
-                        @method('PUT')
-                        @csrf
-                        <div class="row">
-                            <div class="form-group col-12 p-3">
-                                <label>Select User*</label>
-                                <select name="user_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($user as $row)
-                                        <option value="{{ $row->id }}"
-                                            @if ($row->id == $agent->user_id) selected @endif>{{ $row->name }}
-                                            ({{ $row->email }})</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select Project*</label>
-                                <select name="town_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($town as $row)
-                                        <option value="{{ $row->id }}"
-                                            @if ($row->id == $agent->town_id) selected @endif>{{ $row->town }} </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select Project Location*</label>
-                                <select name="sub_town_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($subtown as $row)
-                                        @if ($row->town != null)
-                                            <option value="{{ $row->id }}"
-                                                @if ($row->id == $agent->sub_town_id) selected @endif>({{ $row->town->town }})
-                                                {{ $row->title }}</option>
-                                        @endif
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Select Complaint Type*</label>
-                                <select name="type_id" class="select2-multiple form-control fs-14  h-50px" required>
-                                    @foreach ($type as $row)
-                                        <option value="{{ $row->id }}"
-                                            @if ($row->id == $agent->type_id) selected @endif>({{ $row->title }})
-                                        </option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Agent Profile</label>
-                                <input type="file" class="form-control border-bottom border-1 border-dark" name="avatar"
-                                    value="{{ old('avatar') }}" />
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Description</label>
-                                <textarea class="form-control border-bottom border-1 border-dark" placeholder="Enter Desciption..." name="description"
-                                    value="{{ old('description', $agent->description) }}" required></textarea>
-
-                            </div>
-                            <div class="form-group col-12 p-3">
-                                <label>Address*</label>
-                                <input type="text" class="form-control border-bottom border-1 border-dark"
-                                    placeholder="Enter Address Here..." name="address" required
-                                    value="{{ old('address', $agent->address) }}" />
-                            </div>
-                            <div class="form-group col-12 p-3 text-right">
-                                <button type="submit" class="btn btn-primary">Update</button>
-                                @if (auth()->user()->role == 1)
-                                    <a href="{{ url('/admin/agent-management') }}" class="btn btn-secondary">Cancel</a>
-                                @endif
-                            </div>
+                    @if (auth()->user()->role == 1)
+                        <form role="form" method="POST" action="{{ route('admin.agent-management.update', $agent->id) }}"
+                            enctype="multipart/form-data">
+                        @else
+                            <form role="form" method="POST"
+                                action="{{ route('system.agent-management.update', $agent->id) }}"
+                                enctype="multipart/form-data">
+                    @endif
+                    @method('PUT')
+                    @csrf
+                    <div class="row">
+                        <div class="form-group col-12 p-3">
+                            <label>Select User*</label>
+                            <select name="user_id" class="select2-multiple form-control fs-14  h-50px" required>
+                                @foreach ($user as $row)
+                                    <option value="{{ $row->id }}" @if ($row->id == $agent->user_id) selected @endif>
+                                        {{ $row->name }}
+                                        ({{ $row->email }})
+                                    </option>
+                                @endforeach
+                            </select>
                         </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Select Project*</label>
+                            <select name="town_id" class="select2-multiple form-control fs-14  h-50px" required>
+                                @foreach ($town as $row)
+                                    <option value="{{ $row->id }}" @if ($row->id == $agent->town_id) selected @endif>
+                                        {{ $row->town }} </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Select Project Location*</label>
+                            <select name="sub_town_id" class="select2-multiple form-control fs-14  h-50px" required>
+                                @foreach ($subtown as $row)
+                                    @if ($row->town != null)
+                                        <option value="{{ $row->id }}"
+                                            @if ($row->id == $agent->sub_town_id) selected @endif>({{ $row->town->town }})
+                                            {{ $row->title }}</option>
+                                    @endif
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Select Complaint Type*</label>
+                            <select name="type_id" class="select2-multiple form-control fs-14  h-50px" required>
+                                @foreach ($type as $row)
+                                    <option value="{{ $row->id }}" @if ($row->id == $agent->type_id) selected @endif>
+                                        ({{ $row->title }})
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Agent Profile</label>
+                            <input type="file" class="form-control border-bottom border-1 border-dark" name="avatar"
+                                value="{{ old('avatar') }}" />
+                        </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Description</label>
+                            <textarea class="form-control border-bottom border-1 border-dark" placeholder="Enter Desciption..." name="description"
+                                value="{{ old('description', $agent->description) }}" required></textarea>
+
+                        </div>
+                        <div class="form-group col-12 p-3">
+                            <label>Address*</label>
+                            <input type="text" class="form-control border-bottom border-1 border-dark"
+                                placeholder="Enter Address Here..." name="address" required
+                                value="{{ old('address', $agent->address) }}" />
+                        </div>
+                        <div class="form-group col-12 p-3 text-right">
+                            <button type="submit" class="btn btn-primary">Update</button>
+                            @if (auth()->user()->role == 1)
+                                <a href="{{ url('/admin/agent-management') }}" class="btn btn-secondary">Cancel</a>
+                            @endif
+                        </div>
+                    </div>
 
                     </form>
                 </div>
